87ed2068aefc5cd1ce13006b2514436ff47db422
EJ Sohn <5on.3un.joo@gmail.com>
2017-02-11 19:49:54 +0900

    test for dropzone

--- SkinAnlizer/main/views.py
+++ SkinAnlizer/main/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response
+
+from forms import UploadFileForm
+from models import UploadFile
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_file = UploadFile(file = request.FILES('file'))
+            new_file.save()
+            return HttpResponseRedirect(reverse('main:index'))
+    else :
+        form = UploadFileForm()
+
+    data = {'form': form}
+    return render_to_response('main/index.html', data, context_instance=RequestContext(request))
 
