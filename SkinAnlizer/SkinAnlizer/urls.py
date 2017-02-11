da59f7dea08af28b6dbe4257693b0a8676cdfe89
EJ Sohn <5on.3un.joo@gmail.com>
2017-02-11 17:24:35 +0900

    change root directory revised

--- SkinAnlizer/SkinAnlizer/urls.py
+++ SkinAnlizer/SkinAnlizer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^/', include('main.urls')),
+    url(r'^$', views.home, name='home'),
+    url(r'^main/', include('main.urls')),
     url(r'^admin/', admin.site.urls),
 ]
