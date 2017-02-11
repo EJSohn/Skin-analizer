73199a535bf8e042c474b416aeb975ad57d3445a
EJSohn <5on.3un.7oo@gmail.com>
2017-02-11 12:14:49 +0000

    check for uploaded image

--- SkinAnlizer/SkinAnlizer/settings.py
+++ SkinAnlizer/SkinAnlizer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_URL = '/upload_files/'
