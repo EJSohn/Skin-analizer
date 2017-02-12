3bbf3cd3d31d4747b422268c7c59b727c8c964bc
EJ Sohn <5on.3un.joo@gmail.com>
2017-02-12 17:44:16 +0900

    for merge

--- SkinAnlizer/SkinAnlizer/views.py
+++ SkinAnlizer/SkinAnlizer/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, world.")
+    return redirect('main/')
--- SkinAnlizer/main/models.py
+++ SkinAnlizer/main/models.py
@@ -5,4 +5,4 @@ from django.db import models
 # Create your models here.
 
 class UploadFile(models.Model):
-    file = models.FileField(upload_to='files/%Y/%m/%d')
+    file = models.FileField(upload_to='files/')
